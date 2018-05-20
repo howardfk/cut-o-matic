4d053295478a026b92ae68c53f005e4b79922558
Howard Kim <howardfk@gmail.com>
2018-05-19 22:23:08 -0400

    update to print simple statmnet

--- main.rb
+++ main.rb
@@ -1,12 +1,36 @@
 require 'sketchup.rb'
 
-def setup
-  load '/Users/howard/Scripts/sketchup/setup.rb'
+module Examples
+  module CutOMatic
+
+dirpath = File.dirname(__FILE__)
+path = File.join(dirpath, "/setup.rb")
+
+def setup path
+  #load '/Users/howard/Scripts/sketchup/setup.rb'
+  load path
   include setup
 end
 
-setup
+#setup path
 
-puts model.class
+puts 'test '
+    unless file_loaded?(__FILE__)
 
-puts entities.class
+      menu = UI.menu('Plugins')
+      menu.add_item('wood cuts') {
+        puts 'test '
+        puts path
+        #setup path
+      }
+
+      file_loaded(__FILE__)
+    end
+ 
+#puts model.class
+
+#puts entities.class
+
+  end
+end
+
